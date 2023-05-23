3d00b16316ce5163fe9d10f05928e92fd8ad6974
BrnCalisario <bruno145224@gmail.com>
2023-05-23 11:38:23 -0300

    aulas

--- aula04/algebrica/Program.cs
+++ aula04/algebrica/Program.cs
@@ -3,11 +3,3 @@ using Algebra.Composition;
 
 using static Algebra.FunctionUtil;
 
-Div d = new Div();
-
-d.Add(x);
-d.Add(x + 2);
-
-System.Console.WriteLine(d.Derive()[2]);
-System.Console.WriteLine(d.Derive());
-
