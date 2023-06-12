81b367b74992ba7c5af0f124fe9bbb94807df9c7
BrnCalisario <bruno145224@gmail.com>
2023-06-12 11:34:25 -0300

    Comecando apis

---
+++ aula16/AulaTrevis/Controllers/CpfController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace ProjetoWeb.Controllers;
+ 
+[ApiController]
+[Route("cpf")]
+public class CpfController : ControllerBase
+{
+    [HttpGet("{cpf}")]
+    public ActionResult<Cpf> Get(string cpf)
+    {
+        Cpf result = new Cpf();
+		
+        try
+        {
+            result.Value = cpf;
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+ 
+        return result;
+    }
+
+    [HttpGet("generate/{region}")]
+    public ActionResult<Cpf> Generate(
+        [FromServices]CpfService cpf, int region)
+    {
+        var result = cpf.Generate(region);
+        return result;
+    }
+}
